2e79229da5d3f7f2c4e73c860f1e2d7aa249eb17
Hossam-Elgmmal <109397447+Hossam-Elgmmal@users.noreply.github.com>
2024-04-19 06:58:05 +0200

    set up the project

---
+++ app/src/main/java/com/route/mealsapp/ui/MainContent.kt
@@ -0,0 +1,15 @@
+package com.route.mealsapp.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MainContent() {
+
+}
+
+@Preview
+@Composable
+private fun MainContentPreview() {
+    MainContent()
+}
